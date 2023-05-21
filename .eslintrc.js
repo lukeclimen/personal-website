88a03997de1ea45262a9efa2baab57ee4951eb78
Lucas Climenhage <luke.climenhage@gmail.com>
2023-05-20 20:34:48 -0400

    Initial commit
    Created a NextJS app using `npx create-next-app@latest`, and added
    ESLint and Prettier following the youtube video guide found at
    https://www.youtube.com/watch?v=sH93pQb9bWM

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'latest',
+    },
+  },
+};
