27a734df1ff10c6a220bdde123e2ff9c91e52628
Lucas Climenhage <luke.climenhage@gmail.com>
2023-05-21 15:05:58 -0400

    Remove default components from Storybook

---
+++ stories/Navbar.stories.ts
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar';
+
+export default {
+  title: 'Components/Navbar',
+  component: Navbar,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Default = {
+  args: {},
+};
