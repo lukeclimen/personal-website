7a86d3f82d9a3e49be1eeeeeda4a8ac141ce65ad
Lucas Climenhage <luke.climenhage@gmail.com>
2023-05-27 19:33:20 -0400

    Add dropshadow to Card

--- components/Card.tsx
+++ components/Card.tsx
@@ -20,15 +20,16 @@ export default function Card({ cardTitle, cardText }: cardContent) {
         alt='Headshot'
         className={
           flip
-            ? 'relative z-10 w-60 h-60 md:w-96 md:h-96 translate-x-5 translate-y-5 transition-all'
-            : 'relative z-10 w-60 h-60 md:w-96 md:h-96 -translate-x-5 translate-y-5 md:translate-x-5 md:-translate-y-5 transition-all'
+            ? 'relative z-10 w-60 h-60 md:w-96 md:h-96 translate-x-5 translate-y-5 transition-all shadow-gray-600 shadow-xl'
+            : 'relative z-10 w-60 h-60 md:w-96 md:h-96 -translate-x-5 translate-y-5 md:translate-x-5 md:-translate-y-5 transition-all shadow-gray-600 shadow-xl'
         }
       />
       <div
+        className={('', '')}
         className={
           flip
-            ? 'relative bg-black text-cyan-200 w-60 h-60 md:w-96 md:h-96 p-3 z-20 -translate-x-5 -translate-y-5 transition-all'
-            : 'relative bg-black text-cyan-200 w-60 h-60 md:w-96 md:h-96 p-3 z-20 translate-x-5 -translate-y-5 md:-translate-x-5 md:translate-y-5 transition-all'
+            ? 'relative bg-black text-cyan-200 w-60 h-60 md:w-96 md:h-96 p-3 z-20 -translate-x-5 -translate-y-5 transition-all shadow-black shadow-2xl'
+            : 'relative bg-black text-cyan-200 w-60 h-60 md:w-96 md:h-96 p-3 z-20 translate-x-5 -translate-y-5 md:-translate-x-5 md:translate-y-5 transition-all shadow-black shadow-2xl'
         }
       >
         <h2 className='text-2xl pb-5'>{cardTitle}</h2>
